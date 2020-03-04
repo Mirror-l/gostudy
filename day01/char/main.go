7afa1efd474daa4ebe13bb1206f2f2d39c76845f
tangzuguo <1651504722@qq.com>
2020-03-04 18:01:31 +0800

    123

---
+++ day01/char/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// 遍历字符串
+func traversalString() {
+	s := "hello沙河"
+	for i := 0; i < len(s); i++ { //byte
+		fmt.Printf("%v(%c) ", s[i], s[i])
+	}
+	fmt.Println()
+	for _, r := range s { //rune
+		fmt.Printf("%v(%c) ", r, r)
+	}
+	fmt.Println()
+}
+
+//要修改字符串，需要先将其转换成[]rune或[]byte，完成后再转换为string。无论哪种转换，都会重新分配内存，并复制字节数组。
+func changeString() {
+	s1 := "big"
+	// 强制类型转换
+	byteS1 := []byte(s1)
+	byteS1[0] = 'p'
+	fmt.Println(string(byteS1))
+
+	s2 := "白萝卜"
+	runeS2 := []rune(s2)
+	runeS2[0] = '红'
+	fmt.Println(string(runeS2))
+}
+
+func main() {
+	traversalString()
+	changeString()
+}
--- day01/strings/main.go
+++ day01/strings/main.go
@@ -15,4 +15,23 @@ func main() {
 	// 字符串分割
 	fmt.Println(strings.Split("1,2,3,", ","))
 
+	// 判断是否包含
+	fmt.Println(strings.Contains("abc123", "123"))
+
+	// 前缀判断
+	fmt.Println(strings.HasPrefix("incompatible", "in"))
+
+	// 后缀判断
+	fmt.Println(strings.HasSuffix("incompatible", "able"))
+
+	// 字串第一次出现的位置
+	fmt.Println(strings.Index("i have a computer", "have a"))
+
+	// 字串最后一次出现的位置
+	fmt.Println(strings.LastIndex("hahaha", "h"))
+
+	// 使用指定间隔字符合并字符串
+	array := []string{"abc", "is", "123"}
+	fmt.Println(strings.Join(array, ","))
+
 }
